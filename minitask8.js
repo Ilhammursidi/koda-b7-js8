488be2a5a52739de6515fbaf576b4f4db720a833
Ilhammursidi <imursidi37@gmail.com>
2026-02-26 20:27:24 +0700

    Update minitask8.js

--- minitask8.js
+++ minitask8.js
@@ -1,31 +1,30 @@
 const movie = {
-    id : 1,
-    title : "avatar",
-    image : "link/avatar.jpg",
-    genre :["action","scifi"],
-    cast : ["sam","jack","zoe","netiri"],
-    getName : function() {
-        return`
+  id: 1,
+  title: "avatar",
+  image: "link/avatar.jpg",
+  genre: ["action", "scifi"],
+  cast: ["sam", "jack", "zoe", "netiri"],
+  getName: function () {
+    return `
             judul : ${this.title}
             gambar : ${this.image}
             genre : ${this.genre}
-        `
-    },
-    getNameCast : function() {
-        return`cast : ${this.cast}`
-    },
-    setName : function(newtitle) {
-        return this.title = newtitle
-    },
-    setImage : function(newImage) {
-        return this.image = newImage
-    }
-}
+        `;
+  },
+  getNameCast: function () {
+    return `cast : ${this.cast}`;
+  },
+  setName: function (newtitle) {
+    return (this.title = newtitle);
+  },
+  setImage: function (newImage) {
+    return (this.image = newImage);
+  },
+};
 
 console.log(movie.getName());
 console.log(movie.getNameCast());
 movie.setName("Fast & forius");
 console.log(movie.title);
-movie.setImage('link/car.jpg');
+movie.setImage("link/car.jpg");
 console.log(movie.image);
-
